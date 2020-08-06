2ac9fca2eca14237bb474616af86deea18ca1cb8
SnO2WMaN <sno2wman.official@gmail.com>
2020-08-06 17:31:04 +0900

    refactor: テスト用のDB接続のhostとportを環境変数で設定

--- src/database/store.test.ts
+++ src/database/store.test.ts
@@ -10,8 +10,8 @@ describe("データベースでの実際の挙動の確認", () => {
     await createConnection({
       name: "test",
       type: "mysql",
-      host: "localhost",
-      port: 3306,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
       database: process.env.DB_DATABASE,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
